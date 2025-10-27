979c909f038676978c035aab09e65026f12adf93
Aditya Khandelwal <Adityakhandelwal1205@gmail.com>
2025-10-27 17:04:51 +0530

    updated for review model

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,7 @@
+const { ref } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const listingSchema = new Schema({
   title: { type: String, required: true },
@@ -23,6 +25,18 @@ const listingSchema = new Schema({
           : v,
     },
   },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
