4777f30cb23211abefb7f42c1554707d85e98a22
Aditya Khandelwal <adityakhandelwal1205@gmail.com>
2025-12-15 21:49:23 +0530

    fixed flash message related issues

--- index.js
+++ index.js
@@ -77,5 +77,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}/listings`);
 });
